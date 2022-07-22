1cdc51270eed02f23a93fc76421cbbf69c920bfd
bryan-wijaya <61679677+bryan-wijaya@users.noreply.github.com>
2022-07-22 10:38:47 +0700

    collect ujian and random kunci

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -76,15 +76,15 @@ class StudentController extends ApiController
                 return $this->errorResponse('Something has been wrong.', 400);
             }
             $tmpArr = [];
-            $valid = false;
             if($tipe == 'Tunggal'){
                 if($jenis == "Pilihan Ganda"){
                     $soalpg = DB::table('ujian_has_soal')
                         ->join('soalpg', 'ujian_has_soal.id_soal', '=', 'soalpg.id')
                         ->where('ujian_has_soal.id_ujian','=',$idUjian)
-                        ->select('soalpg.*')
+                        ->select('soalpg.id','soalpg.materi','soalpg.kd','soalpg.soal','soalpg.kelas','soalpg.a','soalpg.b','soalpg.c','soalpg.d','soalpg.e','soalpg.gambarSoal','soalpg.gambarA','soalpg.gambarB','soalpg.gambarC','soalpg.gambarD','soalpg.gambarE','soalpg.kunci_pg','soalpg.kunci_jawaban','soalpg.selected')
                         ->get();
                     for($i = 0; $i < count($soalpg); $i++){
+                        $valid = false;
                         $rand = rand(0,count($soalpg) - 1);
                         while(!$valid){
                             $valid = true;
@@ -92,15 +92,70 @@ class StudentController extends ApiController
                                 if($tmpArr[$j] == $rand){
                                     $valid = false;
                                     break;
-                                }
+                                }   
                             }
-                            $rand +=1;
-                            if($rand > count($soalpg)){
-                                $rand = 0;
+                            if(!$valid){
+                                $rand +=1;
+                                if($rand > count($soalpg) - 1){
+                                    $rand = 0;
+                                }
                             }
                         }
                         array_push($tmpArr, $rand);
                         array_push($retArr,$soalpg[$rand]);
+                    }
+                    for($i = 0; $i < count($retArr); $i++){
+                        if($retArr[$i]->kunci_pg == "E"){
+                            //kunci b
+                            $tmp = $retArr[$i]->a;
+                            $retArr[$i]->a =$retArr[$i]->b;
+                            $retArr[$i]->b =$retArr[$i]->e;
+                            $retArr[$i]->c = $tmp;
+
+                            $tmpGambar = $retArr[$i]->gambarA;
+                            $retArr[$i]->gambarA =$retArr[$i]->gambarB;
+                            $retArr[$i]->gambarB =$retArr[$i]->gambarE;
+                            $retArr[$i]->gambarC = $tmpGambar;
+
+                            $retArr[$i]->kunci_pg = 5;
+                        }else if($retArr[$i]->kunci_pg == "A"){
+                            //kunci d
+                            $tmp = $retArr[$i]->d;
+                            $retArr[$i]->d =$retArr[$i]->a;
+                            $retArr[$i]->c =$retArr[$i]->b;
+                            $retArr[$i]->b =$retArr[$i]->e;
+                            $retArr[$i]->a = $tmp;
+
+                            $tmpGambar = $retArr[$i]->gambarD;
+                            $retArr[$i]->gambarD =$retArr[$i]->gambarA;
+                            $retArr[$i]->gambarC =$retArr[$i]->gambarB;
+                            $retArr[$i]->gambarB =$retArr[$i]->gambarE;
+                            $retArr[$i]->gambarA = $tmpGambar;
+
+                            $retArr[$i]->kunci_pg = 1;
+                        }else if($retArr[$i]->kunci_pg == "B"){
+                            //kunci a
+                            $tmp = $retArr[$i]->a;
+                            $retArr[$i]->a =$retArr[$i]->b;
+                            $retArr[$i]->b =$retArr[$i]->c;
+                            $retArr[$i]->c =$retArr[$i]->d;
+                            $retArr[$i]->d = $tmp;
+
+                            $tmpGambar = $retArr[$i]->gambarA;
+                            $retArr[$i]->gambarA =$retArr[$i]->gambarB;
+                            $retArr[$i]->gambarB =$retArr[$i]->gambarC;
+                            $retArr[$i]->gambarC =$retArr[$i]->gambarD;
+                            $retArr[$i]->gambarD = $tmpGambar;
+
+                            $retArr[$i]->kunci_pg = 2;
+                        }else if($retArr[$i]->kunci_pg == "C"){
+                            //kunci c (tidak di ubah)
+                            $retArr[$i]->kunci_pg = 3;
+                        }else if($retArr[$i]->kunci_pg == "D"){
+                            //kunci d (tidak di ubah)
+                            $retArr[$i]->kunci_pg = 4;
+                        }
+                        $retArr[$i]->e = null;
                     }
                     return $this->successResponse($retArr);
                 }else{
@@ -139,24 +194,34 @@ class StudentController extends ApiController
             }else{
                 return $this->errorResponse('Something has been wrong.', 400);
             }
+            $nilai = 0;
             if($tipe == 'Tunggal'){
                 if($jenis == "Pilihan Ganda"){
+                    $benar = 0;
                     $soalpg = DB::table('ujian_has_soal')
-                        ->join('soalpg', 'ujian_has_soal.id_soal', '=', 'soalpg.id')
-                        ->where('ujian_has_soal.id_ujian','=',$idUjian)
-                        ->select('soalpg.*')
-                        ->get();
-                    for($i=0; $i < count($soalpg); $i++){
-                        for($j=0;$j < count($arr);$j++){
-                            if($soalpg[$i]->id == $arr[$j]['id']){
-                                echo $arr[$j]['selected'] . ' | '   ;
-                                echo $soalpg[$i]->kunci_jawaban . ' - ';
-                                echo $soalpg[$i]->kunci_pg . ' - ' . PHP_EOL;
-                                break;
+                    ->join('soalpg', 'ujian_has_soal.id_soal', '=', 'soalpg.id')
+                    ->where('ujian_has_soal.id_ujian','=',$idUjian)
+                    ->select('soalpg.*')
+                    ->get();
+                    $totalSoal = count($soalpg);
+                    if(isset($request->arr)){
+                        $arr = $request->arr;
+                        for($i = 0; $i < count($arr); $i++){
+                            if($arr[$i]['selected'] != null){
+                                if($this->checkKunci($arr[$i]['kunci_pg'],$arr[$i]['selected'])){
+                                    $benar +=1;
+                                }
                             }
+                            //insert to jawaban siswa
                         }
                     }
-                    exit;
+                    //insert to db nilai
+                    $nilai = $benar / $totalSoal;
+                    $nilai = $nilai * 100;
+                    echo $benar . PHP_EOL;
+                    echo $totalSoal . PHP_EOL;
+                    echo number_format($nilai,2);
+                    return null;
                 }else{
                     $isian = DB::table('ujian_has_soal')
                     ->join('soalisian', 'ujian_has_soal.id_soal', '=', 'soalisian.id')
@@ -169,5 +234,25 @@ class StudentController extends ApiController
             }
         }
         return null;
+    }
+
+    protected function checkKunci($kunci, $jawaban){
+        $jawabanReal = '';
+        if($kunci == 1){
+            $jawabanReal = 'd';
+        }else if($kunci == 2){
+            $jawabanReal = 'a';
+        }else if($kunci == 3){
+            $jawabanReal = 'c';
+        }else if($kunci == 4){
+            $jawabanReal = 'd';
+        }else if($kunci == 5){
+            $jawabanReal = 'b';
+        }
+        if($jawaban == $jawabanReal){
+            return true;
+        }else{
+            return false;
+        }
     }
 }
